82fe39c5a68713b70484e95dbd411e52bd23d729
Matthew Croft <43853954+mattc2020@users.noreply.github.com>
2019-10-22 09:23:15 -0300

    Update Ch06_#1-3.java

--- Ch06_#1-3.java
+++ Ch06_#1-3.java
@@ -21,22 +21,17 @@ import java.util.Scanner;
 public class squareRoot {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+	Scanner in = new Scanner(System.in);
         double number;
         System.out.println("Input Number to Root:");
         number = in.nextDouble();  
-        double x0=6.0;
         double x1 = number/2;
         double x2 = (x1+number/x1)/2;
-      	if(x2%1 == 0) {
-			System.out.println(x2);
-
-		} else {
-			for (int i = 0; i < 1000; i++) {
-		     x2 = (x2+number/x2)/2;
-			}
-		}
-
-
+        
+        while(Math.abs(x2-x1)>0.00001){
+        	x1=x2;
+        	x2 = (x2+number/x2)/2;
+        }
+        System.out.printf("%.2f", x2);
     }
 }
